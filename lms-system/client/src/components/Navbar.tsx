a9b49c8f99dac075e9b7ba9a55d8f917388daaea
Prithivi <prithivi@users.noreply.github.com>
2026-01-27 00:06:46 +0545

    feat: enhance Navbar component with improved styling and user authentication links

--- lms-system/client/src/components/Navbar.tsx
+++ lms-system/client/src/components/Navbar.tsx
@@ -6,30 +6,51 @@ const Navbar = () => {
 
   return (
     <header className="header">
-      <div className="logo">Logo</div>
+      <div className="text-sm md:text-xl">Logo</div>
       <nav>
-        <ul>
+        <ul className="flex-center gap-4">
           <li>
             <Link to="/">Home</Link>
+          </li>
+          <li>
             <Link to="/courses">Courses</Link>
-            {!user ? (
-              <>
-                <Button asChild>
+          </li>
+          {!user ? (
+            <>
+              <li>
+                <Button
+                  asChild
+                  className="bg-jordy-blue-600 hover:bg-jordy-blue-400 text-sm md:text-xl px-3 py-2 md:px-6 md:py-6"
+                >
                   <Link to="/login">Login</Link>
                 </Button>
-                <Button asChild>
+              </li>
+              <li>
+                <Button
+                  asChild
+                  className="bg-transparent border border-jordy-blue-600 text-sm md:text-xl px-3 py-2 md:px-6 md:py-6"
+                >
                   <Link to="/sign-up">SignUp</Link>
                 </Button>
-              </>
-            ) : (
-              <>
-                <div></div>
-                <Button asChild>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <div className="h-12 w-12 rounded-full bg-white">
+                  <img src="" alt="Logo" />
+                </div>
+              </li>
+              <li>
+                <Button
+                  asChild
+                  className="bg-jordy-blue-600 hover:bg-jordy-blue-400 text-sm md:text-xl px-3 py-2 md:px-6 md:py-6"
+                >
                   <Link to="/login">Logout</Link>
                 </Button>
-              </>
-            )}
-          </li>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
