838805339340bb0954532e9bf0173b497a4ae54b
Prithivi <prithivi@users.noreply.github.com>
2026-01-27 21:48:55 +0545

    feat: add cookie parser and CORS middleware for enhanced request handling

--- lms-system/server/src/app.ts
+++ lms-system/server/src/app.ts
@@ -1,10 +1,19 @@
 import express, { type Express } from "express";
 import routes from "./routes/index.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use(routes);
 
