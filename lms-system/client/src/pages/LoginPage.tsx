0c9bc2946d3e51415fee3fab31240293ca5a87ed
Prithivi <prithivi@users.noreply.github.com>
2026-01-27 21:58:50 +0545

    feat: implement login functionality with form handling and success/error notifications

--- lms-system/client/src/pages/LoginPage.tsx
+++ lms-system/client/src/pages/LoginPage.tsx
@@ -1,13 +1,50 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "react-router-dom";
+import { BASE_URL } from "@/constants";
+import axios from "axios";
+import React, { use, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const [user, setuser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setuser((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`${BASE_URL}/login`, user, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.data?.success) {
+        navigate("/dashboard");
+        toast.success(response.data.message || "Login successful!");
+      } else {
+        toast.error(response.data.message || "Login failed. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
   return (
     <div className="w-full h-screen fixed overflow-hidden flex-center ">
       <div className="form-container login">
         <p className="title">Log in</p>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-2">
             <Label htmlFor="email">Email</Label>
             <Input
@@ -15,6 +52,9 @@ const LoginPage = () => {
               type="email"
               className="input"
               placeholder="Email"
+              name="email"
+              value={user.email}
+              onChange={handleChange}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -24,6 +64,9 @@ const LoginPage = () => {
               type="password"
               className="input"
               placeholder="Password"
+              name="password"
+              value={user.password}
+              onChange={handleChange}
             />
           </div>
           <button className="form-btn bg-border" type="submit">
