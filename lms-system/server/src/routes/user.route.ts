26bd7ebf790ff36200da90a0b75c304d543da582
Prithivi <prithivi@users.noreply.github.com>
2026-01-27 17:49:55 +0545

    feat: implement user registration, login, and logout routes

--- lms-system/server/src/routes/user.route.ts
+++ lms-system/server/src/routes/user.route.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import { register, login, logout } from "../controller/user.controller.js";
 
 const router: Router = Router();
 
-router.route("/register").post();
+router.route("/register").post(register);
+
+router.route("/login").post(login);
+
+router.route("/logout").post(logout);
 
 export default router;
