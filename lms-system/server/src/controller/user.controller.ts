fbc7be55567f5d66ed4441686a73e72382907a38
Prithivi <prithivi@users.noreply.github.com>
2026-01-27 17:18:39 +0545

    feat: refactor user registration and login functions with JWT implementation

--- lms-system/server/src/controller/user.controller.ts
+++ lms-system/server/src/controller/user.controller.ts
@@ -1,7 +1,9 @@
-import type { NextFunction, Request, Response } from "express";
+import type { Request, Response } from "express";
 import { User } from "../model/user.model.js";
+import jwt from "jsonwebtoken";
+import { env } from "../config/env.config.js";
 
-export async function registeruser(req: Request, res: Response) {
+export async function register(req: Request, res: Response) {
   try {
     const { name, email, password, role } = req.body as RegisterBody;
 
@@ -27,6 +29,55 @@ export async function registeruser(req: Request, res: Response) {
     });
   } catch (error) {
     console.log("Error in user registration:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function login(req: Request, res: Response) {
+  try {
+    const { email, password } = req.body as LoginBody;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      env.jwtSecret as string,
+      {
+        expiresIn: "1d",
+      },
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: env.nodeEnv === "production",
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
+
+    return res.status(200).json({
+      message: "User logged in successfully",
+      user,
+      token,
+    });
+  } catch (error) {
+    console.log("Error in user login:", error);
     return res.status(500).json({ message: "Internal server error" });
   }
 }
