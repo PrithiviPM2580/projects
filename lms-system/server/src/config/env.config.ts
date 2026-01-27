90b29693a9dba78be3b1cb9ef387a1bf95d8fa1c
Prithivi <prithivi@users.noreply.github.com>
2026-01-27 17:18:57 +0545

    feat: add jwtSecret and nodeEnv to environment configuration

--- lms-system/server/src/config/env.config.ts
+++ lms-system/server/src/config/env.config.ts
@@ -3,4 +3,6 @@ import "dotenv/config";
 export const env = {
   port: process.env.PORT || 5000,
   databaseUrl: process.env.DATABASE_URL || "",
+  jwtSecret: process.env.JWT_SECRET,
+  nodeEnv: process.env.NODE_ENV || "development",
 };
