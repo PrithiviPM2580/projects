14d892cfdd3e3b6b8c8ac64ebb9bd11dc9bd16f5
Prithivi <prithivi@users.noreply.github.com>
2026-01-27 14:06:09 +0545

    feat: enhance Course component styles for improved responsiveness

--- lms-system/client/src/components/Course.tsx
+++ lms-system/client/src/components/Course.tsx
@@ -4,8 +4,10 @@ import { COURSES } from "@/constants";
 const Course = () => {
   return (
     <section className="padding w-full min-h-screen flex flex-col gap-4">
-      <h1 className="text-center font-semibold text-2xl">Courses</h1>
-      <p className="text-center text-gray-600 text-sm md:text-base">
+      <h1 className="text-center font-semibold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
+        Courses
+      </h1>
+      <p className="text-center text-gray-600 text-sm md:text-base lg:text-xl">
         Explore our wide range of courses designed to help you learn and grow.
       </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-8">
