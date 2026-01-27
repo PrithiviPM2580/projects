8822abe1a9f702d1b567c9bf933dc0cac0a14bea
Prithivi <prithivi@users.noreply.github.com>
2026-01-27 11:32:10 +0545

    feat: add routing for Course, Signup, and Login pages in AppRoute component

--- lms-system/client/src/routes/AppRoute.tsx
+++ lms-system/client/src/routes/AppRoute.tsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import CoursePage from "@/pages/CoursePage";
+import SignupPage from "@/pages/SignupPage";
+import LoginPage from "@/pages/LoginPage";
 
 const AppRoute = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/courses" element={<CoursePage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 };
