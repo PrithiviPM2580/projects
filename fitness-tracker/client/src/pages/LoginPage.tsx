4e5239b3094fbbaaf34bd3661bc28405efdcdafd
Prithivi <prithivi@users.noreply.github.com>
2026-01-30 15:46:20 +0545

    feat: enhance LoginPage with user feedback and password visibility toggle

--- fitness-tracker/client/src/pages/LoginPage.tsx
+++ fitness-tracker/client/src/pages/LoginPage.tsx
@@ -13,13 +13,20 @@ import {
 } from "@/components/ui/card";
 import {
   Field,
-  FieldDescription,
   FieldError,
   FieldGroup,
   FieldLabel,
 } from "@/components/ui/field";
 import { Button } from "@/components/ui/button";
-import { MailIcon } from "lucide-react";
+import {
+  MailIcon,
+  AtSignIcon,
+  LockIcon,
+  EyeOffIcon,
+  EyeIcon,
+} from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import useAppContext from "@/hooks/useAppContext";
 
 const formSchema = z.object({
   userName: z.string().min(3, "Username must be at least 3 characters"),
@@ -32,6 +39,9 @@ type Auth = "login" | "sign-up";
 const LoginPage = () => {
   const [auth, setAuth] = useState<Auth>("login");
   const [showPassword, setShowPassword] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const navigate = useNavigate();
+  const { user, login, signup } = useAppContext();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -42,78 +52,98 @@ const LoginPage = () => {
     },
   });
 
-  function onSubmit(data: z.infer<typeof formSchema>) {
-    toast("You submitted the following values:", {
-      description: (
-        <pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
-          <code>{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-      position: "bottom-right",
-      classNames: {
-        content: "flex flex-col gap-2",
-      },
-      style: {
-        "--border-radius": "calc(var(--radius)  + 4px)",
-      } as React.CSSProperties,
-    });
+  async function onSubmit(data: z.infer<typeof formSchema>) {
+    setIsSubmitting(true);
+    if (auth === "login") {
+      await login({
+        email: data.email,
+        password: data.password,
+      });
+    } else {
+      await signup({
+        username: data.userName,
+        email: data.email,
+        password: data.password,
+      });
+    }
+    toast.success(
+      `Successfully ${auth === "login" ? "logged in" : "registered"}`,
+    );
+    setIsSubmitting(false);
+
+    navigate("/dashboard");
   }
 
   return (
     <section className="login-page-container ">
       <Card className="login-form-container">
-        <CardTitle>{auth === "login" ? "Login" : "Register"}</CardTitle>
-        <CardDescription>
+        <CardTitle className="text-center theme-text heading-h2">
+          {auth === "login" ? "Login" : "Register"}
+        </CardTitle>
+        <CardDescription className="theme-text text-center">
           {auth === "login"
             ? "Please enter your credentials to login."
             : "Fill in the details to create a new account."}
         </CardDescription>
-        <CardContent>
+        <CardContent className="w-full">
           <form
             id="form-rhf-demo"
             onSubmit={form.handleSubmit(onSubmit)}
             className="login-form"
           >
-            <FieldGroup>
-              <Controller
-                name="userName"
-                control={form.control}
-                render={({ field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="form-rhf-demo-userName">
-                      Username
-                    </FieldLabel>
-                    <div className="relative ">
-                      <MailIcon className="absolute top-1/2 left-2 -translate-y-1/2" />
-                      <Input
-                        {...field}
-                        id="form-rhf-demo-userName"
-                        aria-invalid={fieldState.invalid}
-                        placeholder="Please enter your username"
-                        autoComplete="off"
-                        className="login-input"
-                      />
-                    </div>
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
+            <FieldGroup className="gap-4">
+              {auth === "sign-up" && (
+                <Controller
+                  name="userName"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field data-invalid={fieldState.invalid}>
+                      <FieldLabel
+                        htmlFor="form-rhf-demo-userName"
+                        className="theme-text"
+                      >
+                        Username
+                      </FieldLabel>
+                      <div className="relative ">
+                        <AtSignIcon className="absolute top-1/2 left-4 -translate-y-1/2 w-5 h-5 theme-text" />
+                        <Input
+                          {...field}
+                          id="form-rhf-demo-userName"
+                          aria-invalid={fieldState.invalid}
+                          placeholder="Please enter your username"
+                          autoComplete="off"
+                          className="login-input theme-text"
+                        />
+                      </div>
+                      {fieldState.invalid && (
+                        <FieldError errors={[fieldState.error]} />
+                      )}
+                    </Field>
+                  )}
+                />
+              )}
               <Controller
                 name="email"
                 control={form.control}
                 render={({ field, fieldState }) => (
                   <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="form-rhf-demo-email">Email</FieldLabel>
-                    <Input
-                      {...field}
-                      id="form-rhf-demo-email"
-                      aria-invalid={fieldState.invalid}
-                      placeholder="Please enter your email"
-                      autoComplete="off"
-                      className="login-input"
-                    />
+                    <FieldLabel
+                      htmlFor="form-rhf-demo-email"
+                      className="theme-text"
+                    >
+                      Email
+                    </FieldLabel>
+                    <div className="relative ">
+                      <MailIcon className="absolute top-1/2 left-4 -translate-y-1/2 w-5 h-5 theme-text" />
+                      <Input
+                        {...field}
+                        id="form-rhf-demo-email"
+                        aria-invalid={fieldState.invalid}
+                        placeholder="Please enter your email"
+                        autoComplete="off"
+                        className="login-input theme-text"
+                      />
+                    </div>
                     {fieldState.invalid && (
                       <FieldError errors={[fieldState.error]} />
                     )}
@@ -125,17 +155,37 @@ const LoginPage = () => {
                 control={form.control}
                 render={({ field, fieldState }) => (
                   <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="form-rhf-demo-password">
+                    <FieldLabel
+                      htmlFor="form-rhf-demo-password"
+                      className="theme-text"
+                    >
                       Password
                     </FieldLabel>
-                    <Input
-                      {...field}
-                      id="form-rhf-demo-password"
-                      aria-invalid={fieldState.invalid}
-                      placeholder="Please enter your password"
-                      autoComplete="off"
-                      className="login-input"
-                    />
+                    <div className="relative">
+                      <LockIcon className="absolute top-1/2 left-4 -translate-y-1/2 w-5 h-5 theme-text" />
+                      <Input
+                        {...field}
+                        id="form-rhf-demo-password"
+                        aria-invalid={fieldState.invalid}
+                        placeholder="Please enter your password"
+                        autoComplete="off"
+                        className="login-input theme-text"
+                        type={showPassword ? "text" : "password"}
+                      />
+                      <Button
+                        type="button"
+                        variant="link"
+                        size="icon-sm"
+                        className="absolute right-2 top-1/2 -translate-y-1/2"
+                        onClick={() => setShowPassword((p) => !p)}
+                      >
+                        {showPassword ? (
+                          <EyeOffIcon className="w-5 h-5 theme-text cursor-pointer" />
+                        ) : (
+                          <EyeIcon className="w-5 h-5 theme-text cursor-pointer" />
+                        )}
+                      </Button>
+                    </div>
                     {fieldState.invalid && (
                       <FieldError errors={[fieldState.error]} />
                     )}
@@ -143,18 +193,41 @@ const LoginPage = () => {
                 )}
               />
             </FieldGroup>
+            <Button
+              type="submit"
+              className="login-button"
+              disabled={isSubmitting}
+            >
+              {isSubmitting
+                ? "Submitting..."
+                : auth === "login"
+                  ? "Login"
+                  : "Sign Up"}
+            </Button>
           </form>
         </CardContent>
-        <CardFooter>
+        <CardFooter className="justify-center">
           {auth === "login" ? (
-            <p>
+            <p className="theme-text">
               Dont't have the account ?{" "}
-              <Button onClick={() => setAuth("sign-up")}>Sign up</Button>
+              <Button
+                variant="link"
+                className="text-mint-green-600 font-semibold px-1"
+                onClick={() => setAuth("sign-up")}
+              >
+                Sign up
+              </Button>
             </p>
           ) : (
-            <p>
+            <p className="theme-text">
               Already have an account?{" "}
-              <Button onClick={() => setAuth("login")}>Login</Button>
+              <Button
+                variant="link"
+                className="text-mint-green-600 font-semibold px-1"
+                onClick={() => setAuth("login")}
+              >
+                Login
+              </Button>
             </p>
           )}
         </CardFooter>
