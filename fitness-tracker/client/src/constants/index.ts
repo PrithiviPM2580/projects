e98497a5a174db32d9597db0774e67b08d4cd25f
Prithivi <prithivi@users.noreply.github.com>
2026-01-31 10:33:55 +0545

    feat: add navigation items with icons for main routes

---
+++ fitness-tracker/client/src/constants/index.ts
@@ -0,0 +1,24 @@
+import { ActivityIcon, HomeIcon, UserIcon, UtensilsIcon } from "lucide-react";
+
+export const navItems = [
+  {
+    path: "/",
+    label: "Home",
+    icon: HomeIcon,
+  },
+  {
+    path: "/food",
+    label: "Food",
+    icon: UtensilsIcon,
+  },
+  {
+    path: "/activity",
+    label: "Activity",
+    icon: ActivityIcon,
+  },
+  {
+    path: "/profile",
+    label: "Profile",
+    icon: UserIcon,
+  },
+];
