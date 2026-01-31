1543b7269f89467efd56dd7ed0bf6351b3b1348c
Prithivi <prithivi@users.noreply.github.com>
2026-01-31 10:33:37 +0545

    feat: implement layout structure with Sidebar and Outlet in LayoutPage

--- fitness-tracker/client/src/pages/LayoutPage.tsx
+++ fitness-tracker/client/src/pages/LayoutPage.tsx
@@ -1,9 +1,13 @@
+import Sidebar from "@/components/Sidebar";
 import { Outlet } from "react-router-dom";
 
 const LayoutPage = () => {
   return (
-    <div>
-      <Outlet />
+    <div className="layout-container">
+      <Sidebar />
+      <div className="flex-1 overflow-y-scroll">
+        <Outlet />
+      </div>
     </div>
   );
 };
