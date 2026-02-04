b1d8a26d9e31f466c5222e016b949fd49592afa9
Prithivi <prithivi@users.noreply.github.com>
2026-02-05 03:53:46 +0545

    fix: update API routes and CORS configuration for improved security and consistency

--- real-time-sport-broadcast/backend/src/index.ts
+++ real-time-sport-broadcast/backend/src/index.ts
@@ -13,26 +13,28 @@ const HOST = process.env.HOST || "0.0.0.0";
 const app = express();
 const server = http.createServer(app);
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 app.all("/api/auth/{*splat}", toNodeHandler(auth));
 
 // JSON middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
 
 // Root GET route
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Welcome to Real-Time Sport Broadcast API!" });
 });
 
-app.use("/matches", matchRouter);
-app.use("/matches/:id/commentary", commentryRouter);
+app.use("/api/matches", matchRouter);
+app.use("/api/matches/:id/commentary", commentryRouter);
 
 const { broadcastMatchCreated, broadcastCommentary } =
   setupWebSocketServer(server);
