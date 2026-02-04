45774d597544ec769e178561a0c3570ac2348d2d
Prithivi <prithivi@users.noreply.github.com>
2026-02-04 18:13:41 +0545

    feat: update ThemeProvider defaultTheme to dark and add Toaster component

--- real-time-sport-broadcast/frontend/src/main.tsx
+++ real-time-sport-broadcast/frontend/src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
+import { Toaster } from "./components/ui/sonner.tsx";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <App />
+      <Toaster />
     </ThemeProvider>
   </StrictMode>,
 );
