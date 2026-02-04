eb3fd7c1a100487e46f752711a004b77065fcc45
Prithivi <prithivi@users.noreply.github.com>
2026-02-04 15:51:49 +0545

    feat: refactor AuthPage component to use UI components and improve structure

--- real-time-sport-broadcast/frontend/src/pages/AuthPage.tsx
+++ real-time-sport-broadcast/frontend/src/pages/AuthPage.tsx
@@ -1,22 +1,35 @@
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import type { Auth } from "@/types";
+import { useState } from "react";
+
 const AuthPage = () => {
+  const [auth, setAuth] = useState<Auth>("login");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
   return (
     <section className="wrapper">
-      <div className="card-switch">
-        <label className="switch">
-          <input type="checkbox" className="toggle" />
+      <Card className="card-switch">
+        <Label className="switch">
+          <Input type="checkbox" className="toggle" />
           <span className="slider"></span>
           <span className="card-side"></span>
           <div className="flip-card__inner">
             <div className="flip-card__front">
               <div className="title">Log in</div>
               <form className="flip-card__form" action="">
-                <input
+                <Input
                   className="flip-card__input"
                   name="email"
                   placeholder="Email"
                   type="email"
                 />
-                <input
+                <Input
                   className="flip-card__input"
                   name="password"
                   placeholder="Password"
@@ -28,18 +41,18 @@ const AuthPage = () => {
             <div className="flip-card__back">
               <div className="title">Sign up</div>
               <form className="flip-card__form" action="">
-                <input
+                <Input
                   className="flip-card__input"
                   placeholder="Name"
                   type="name"
                 />
-                <input
+                <Input
                   className="flip-card__input"
                   name="email"
                   placeholder="Email"
                   type="email"
                 />
-                <input
+                <Input
                   className="flip-card__input"
                   name="password"
                   placeholder="Password"
@@ -49,8 +62,8 @@ const AuthPage = () => {
               </form>
             </div>
           </div>
-        </label>
-      </div>
+        </Label>
+      </Card>
     </section>
   );
 };
