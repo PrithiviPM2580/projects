fa22fd7338346e8212c4f83f9ba17648fd21ef57
Prithivi <prithivi@users.noreply.github.com>
2026-02-05 21:33:38 +0545

    feat: remove BrowserRouter wrapper from AppRoutes component

--- real-time-video-calling/client/src/routes/AppRoutes.tsx
+++ real-time-video-calling/client/src/routes/AppRoutes.tsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Auth from "@/pages/auth/Auth";
 import Dashboard from "@/pages/dashboard/Dashboard";
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="signup" element={<Auth type="signup" />} />
-        <Route path="login" element={<Auth type="login" />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/signup" element={<Auth type="signup" />} />
+      <Route path="/login" element={<Auth type="login" />} />
+    </Routes>
   );
 };
 
