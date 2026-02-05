23f06bc8fb4873629fb226afc9fb643ead42da0e
Prithivi <prithivi@users.noreply.github.com>
2026-02-05 14:22:18 +0545

    feat(server): enhance CORS configuration and implement database connection on server start

--- real-time-video-calling/server/src/index.ts
+++ real-time-video-calling/server/src/index.ts
@@ -2,11 +2,18 @@ import "dotenv/config";
 import express, { type Express } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import connectToDatabase from "./db/db";
 
 const app: Express = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  }),
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +22,14 @@ app.get("/", (req, res) => {
   res.send("Real-Time Video Calling Server is running!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+(async () => {
+  try {
+    await connectToDatabase();
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start the server:", error);
+    process.exit(1);
+  }
+})();
