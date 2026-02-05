2453a02ed30bec781f364a94433c1a51debbcdf1
Prithivi <prithivi@users.noreply.github.com>
2026-02-05 19:10:15 +0545

    feat: implement user fetching logic with error handling and loading state

--- real-time-video-calling/client/src/App.tsx
+++ real-time-video-calling/client/src/App.tsx
@@ -1,6 +1,41 @@
 import AppRoutes from "./routes/AppRoutes";
+import { useEffect } from "react";
+import { useAuth } from "./hooks/useAuth";
+import apiClient from "./lib/api-client";
+import { Spinner } from "./components/ui/spinner";
 
 const App = () => {
+  const { setUser, loading, setLoading, error, setError } = useAuth();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await apiClient.get("/auth/me");
+
+        if (!response.data) {
+          setUser(null);
+          setLoading(false);
+          return;
+        }
+        setUser(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+        setError(error);
+        setLoading(false);
+        setUser(null);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
   return <AppRoutes />;
 };
 
