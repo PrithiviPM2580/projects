f20221949b506293f136db8b7c81676c84909946
Prithivi <prithivi@users.noreply.github.com>
2026-02-05 09:46:39 +0545

    feat: implement conditional rendering in Commentary component for match selection

--- real-time-sport-broadcast/frontend/src/components/Commentary.tsx
+++ real-time-sport-broadcast/frontend/src/components/Commentary.tsx
@@ -1,5 +1,34 @@
+import { Video } from "lucide-react";
+import Header from "./Header";
+
 const Commentary = () => {
-  return <div className="bg-red-400 flex-1">Commentary</div>;
+  const ismatchSelected = true;
+  if (!ismatchSelected) {
+    return (
+      <div className="flex-1 flex-center w-full h-svh rounded-4xl border-2 border-dotted border-black">
+        <div className="flex flex-col gap-6 items-center">
+          <div className="w-20 h-20 flex-center rounded-full border-2 border-black bg-sport-300">
+            <Video className="size-8" />
+          </div>
+          <h1 className="text-2xl font-bold">No Match Selected</h1>
+          <p className="w-[60%] text-center text-slate-500">
+            Select a match from the list to view live commentary and real-time
+            updates.
+          </p>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="bg-red-400 flex-1 card p-4">
+      <Header className="flex-between">
+        <h1 className="font-bold text-xl">Live Commentary</h1>
+        <p className="px-3 py-0.5 bg-slate-100 rounded-2xl border-2 border-black btn">
+          Real Time
+        </p>
+      </Header>
+    </div>
+  );
 };
 
 export default Commentary;
